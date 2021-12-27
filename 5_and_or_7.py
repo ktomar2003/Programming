4ef937c7c0ccf4848baff0998bc8c79b9a877d9c
ktomar2003 <92124480+ktomar2003@users.noreply.github.com>
2021-12-27 22:39:51 +0530

    Update 5_and_or_7.py

--- 5_and_or_7.py
+++ 5_and_or_7.py
@@ -1,25 +1,50 @@
 def five_or_seven(n):
-    c,c1,c2=0,0,0    
-    while n%5==0:
-        n=n-5
-        c1+=1
-        return c1
-    while n%7==0:
-        n=n-7
-        c2+=1
-        return c2
+    print("FOR",n,":")
+    five_s,seven_s=0,0    
+    if n%5==0:
+        n1=n//5
+        print("No. of 5's in",n,"are :",n1)
+        print("No. of 7's in",n,"is : 0")
+    elif n%7==0:
+        n2=n//7
+        print("No. of 5's in",n,"is : 0")
+        print("No. of 7's in",n,"are :",n2)
     else:
-        limit=[7,5]
-        for i in range(2):
-            while n>=limit[i]:
-                n-=limit[i]
-                c+=1
-            if n==0:
-                break
-        return c
+    while n!=0:
+        n-=5
+        five_s+=1
+        if n%7==0:
+            seven_s=n//7
+        else:
+            n-=5
+            five_s+=1
+            if n%7==0:
+                seven_s=n//7
+            else:
+                n-=5
+                five_s+=1
+                if n%7==0:
+                    seven_s=n//7
+                else:
+                    n-=5
+                    five_s+=1
+                    if n%7==0:
+                        seven_s=n//7
+                    else:
+                        n-=5
+                        five_s+=1
+                        if n%7==0:
+                            seven_s=n//7
+                    
+    print("No. of 5's in",n,"are :",five_s)
+    print("No. of 7's in",n,"are :",seven_s)
+
 
 T=int(input())
 for _ in range(T):
     n=int(input())
-    ans=five_or_seven(n)
-    print(ans)
+    if n>=24:
+        five_or_seven(n)
+    else:
+        print("FOR",n,":")
+        print("Please Enter a Number Greater than or Equal to 24")
