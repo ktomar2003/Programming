2cad4a56d4b9917d11450ab6cf470401b9f8968b
ktomar2003 <92124480+ktomar2003@users.noreply.github.com>
2022-01-03 23:41:42 +0530

    Update 5_and_or_7.py

--- 5_and_or_7.py
+++ 5_and_or_7.py
@@ -10,35 +10,16 @@ def five_or_seven(n):
         print("No. of 5's in",n,"is : 0")
         print("No. of 7's in",n,"are :",n2)
     else:
-    while n!=0:
-        n-=5
-        five_s+=1
-        if n%7==0:
-            seven_s=n//7
-        else:
-            n-=5
+        n_dup1=n
+        while n_dup1!=0:
+            n_dup1-=5
             five_s+=1
-            if n%7==0:
-                seven_s=n//7
-            else:
-                n-=5
-                five_s+=1
-                if n%7==0:
-                    seven_s=n//7
-                else:
-                    n-=5
-                    five_s+=1
-                    if n%7==0:
-                        seven_s=n//7
-                    else:
-                        n-=5
-                        five_s+=1
-                        if n%7==0:
-                            seven_s=n//7
-                    
-    print("No. of 5's in",n,"are :",five_s)
-    print("No. of 7's in",n,"are :",seven_s)
-
+            if n_dup1%7==0:
+                while n_dup1!=0:
+                    n_dup1-=7
+                    seven_s+=1
+        print("No. of 5's in",n,"are :",five_s)
+        print("No. of 7's in",n,"are :",seven_s)
 
 T=int(input())
 for _ in range(T):
