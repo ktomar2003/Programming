9bdec0d411fd42d3dd3fc0547da6fb4bf46a70e4
ktomar2003 <92124480+ktomar2003@users.noreply.github.com>
2022-01-06 00:28:24 +0530

    Update string_and_patterns.c

--- string_and_patterns.c
+++ string_and_patterns.c
@@ -18,6 +18,33 @@ int main(void)
 return 0;
 }
 **************************
+#include<stdio.h>
+int main()
+{
+    int space;
+    for(int i=10;i>=0;i--)
+    {
+        for(space=1;space<=11-i;space++)
+            printf(" ");
+        for(int j=1;j<=i;j++)
+        {
+            printf("  %d",i);
+        }
+    printf("\n");
+    }
+    for(int i=1;i<=10;i++)
+    {
+        for(space=1;space<=11-i;space++)
+            printf(" ");
+        for(int j=1;j<=i;j++)
+        {
+            printf("  %d",i);
+        }
+        printf("\n");
+    }
+return 0;
+}
+**************************
 for(int i=10;i>=0;i--)    // FOR REVERSE
 {
     for(int j=1;j<=i;j++)
