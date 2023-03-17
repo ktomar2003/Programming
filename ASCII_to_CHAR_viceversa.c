4fb4def3ee8e810ba256d8ceac833b4f0c8c4311
ktomar2003 <92124480+ktomar2003@users.noreply.github.com>
2023-03-17 10:32:14 +0530

    Create ASCII_to_CHAR_viceversa.c

---
+++ ASCII_to_CHAR_viceversa.c
@@ -0,0 +1,20 @@
+//IN C
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char c;
+    scanf("%c",&c);
+    printf("%d",c);
+    return 0;
+}
+/*
+#IN PYTHON
+#ASCII value
+c=input()
+print(ord(c))
+
+#CHARACTER FROM ASCII VALUE
+c=int(input())
+print(chr(c))
+*/
