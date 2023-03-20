cac9b515fe6b4b239bc9fb6f66254d600d380d49
ktomar2003 <92124480+ktomar2003@users.noreply.github.com>
2023-03-20 12:33:11 +0530

    Update algorithms_codes.py

--- algorithms_codes.py
+++ algorithms_codes.py
@@ -19,3 +19,20 @@ if i<n:
     print(i)
 else:
     print(-1)
+
+----------------------------------------------
+    
+n,c=6,0
+flag=0
+a=[53,25,38,20,15,28]
+for i in range(n):
+    for j in range(i+1,n):
+        if a[i]==a[j]:
+            flag=1
+            break
+        c+=1
+#print(c)
+if flag==1:
+    print("Equal elements found")
+else:
+    print("Equal elements NOT found")
