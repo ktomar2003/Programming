fb4fe0b08e50d5d543e564d0f8e6911f9607a3cd
ktomar2003 <92124480+ktomar2003@users.noreply.github.com>
2023-04-05 14:57:53 +0530

    Create removing_a_cycle_in_ll.c

---
+++ removing_a_cycle_in_ll.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node node;
+
+node *createNode(int ele)
+{
+    node *p;
+    p = (node *)malloc(sizeof(node));
+    p->data=ele;
+    p->next=NULL;
+    return p;
+}
+
+void insert(node **head, int ele)
+{
+    node *p;
+    p=createNode(ele);
+    *head=p;
+    p=p->next;
+    p->next=*head;
+}
+
+
+
+int main(void)
+{
+    node *head=NULL;
+    int ele=80,ele1=70,ele2=60;
+    
+    insert(&head,ele);
+    insert(&head,ele1);
+    insert(&head,ele2);
+    
+    
+    return 0;
+}
